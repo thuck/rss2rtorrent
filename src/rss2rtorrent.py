829889c8c5966fe2c711715411c9934cce95b1d1
Denis Doria <denisdoria@gmail.com>
2012-04-27 14:33:00 -0300

    Create lock to avoid two programs to run at the same time.

--- src/rss2rtorrent.py
+++ src/rss2rtorrent.py
@@ -4,7 +4,7 @@ import os
 import os.path
 import sys
 import re
-import collections
+import socket
 
 def save_magnetic_links(directory, series):
 
@@ -16,6 +16,10 @@ def save_magnetic_links(directory, series):
         if not os.path.exists(filename):
             with open(filename, 'w') as magnet_file:
                 magnet_file.write('d10:magnet-uri%s:%se' % (len(serie['magneturi']), serie['magneturi']))
+
+def save_torrent_file(directory, series):
+    #TODO:create the .torrent file
+    pass
 
 def get_series(series, rss):
 
@@ -29,7 +33,21 @@ def get_series(series, rss):
 
     return series_list
 
+def get_lock():
+    #Avoid two scrips to run at the same time
+    #Got this example from: http://stackoverflow.com/a/7758075
+    lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+    try:
+        lock_socket.bind('\0' + 'rss2rtorrent')
+
+    except socket.error:
+        #This error should be logged
+        sys.exit(1)
+
+
 if __name__ == '__main__':
+
+    get_lock()
     config = ConfigParser.RawConfigParser()
     config.read(os.path.expanduser('~/.config/rss2rtorrent/feed.cfg'))
     watch_directory = config.get('rtorrent', 'watch_directory')
@@ -43,6 +61,4 @@ if __name__ == '__main__':
         rss = config.get(section, 'rss')
         save_magnetic_links(watch_directory, get_series(series, rss))
 
-
-
-
+    sys.exit(0)
